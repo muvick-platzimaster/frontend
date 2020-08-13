479050482ddf1c47e867a771d84a267b8ab2287c
Angelo Zambrano <angelozam17@gmail.com>
2020-08-13 18:48:41 -0500

    [ADD] Support Stylus

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Home from "./pages/Home";
 
 /* Styles */
 import { GlobalStyle } from "./styles/GlobalStyle";
+import "./styles/main.styl";
 
 const App: React.FC = (): JSX.Element => {
    return (
--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,48 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-/* const MiniCssExtractPlugin = require("mini-css-extract-plugin"); */
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
    entry: path.resolve(__dirname, "src", "index.tsx"),
    output: {
       path: path.resolve(__dirname, "dist"),
-      filename: "bundle.js",
+      filename: "bundle.js"
    },
    resolve: {
-      extensions: [".ts", ".tsx", ".jsx", ".js"],
+      extensions: [".ts", ".tsx", ".jsx", ".js"]
    },
    devServer: {
-      historyApiFallback: true,
+      historyApiFallback: true
    },
    module: {
       rules: [
          {
             test: /\.tsx?$/,
             use: "babel-loader",
-            exclude: /node_modules/,
+            exclude: /node_modules/
          },
-      ],
+         {
+            test: /\.styl$/,
+            use: [MiniCssExtractPlugin.loader, "css-loader", "stylus-loader"]
+         }
+      ]
    },
    optimization: {
       splitChunks: {
          name: "commons",
          chunks: "all",
          minSize: 0,
-         filename: "[name].js",
-      },
+         filename: "[name].js"
+      }
    },
    plugins: [
       new HtmlWebpackPlugin({
          template: path.resolve(__dirname, "public", "index.html"),
-         filename: "index.html",
+         filename: "index.html"
       }),
-   ],
+      new MiniCssExtractPlugin({
+         chunkFilename: "[name].chunk.css",
+         filename: "[name].css"
+      })
+   ]
 };
